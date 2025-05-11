5de2f8111540b6adeb02618284cb1de937e0ef49
herba1 <burnedcard77@gmail.com>
2025-05-11 01:39:45 -0700

    lenis context

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,34 @@
-'use client'
-import Image from "next/image";
-import { useGSAP } from "@gsap/react";
-import { gsap } from "gsap";
-    
-import { Observer } from "gsap/Observer";
-import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-// ScrollSmoother requires ScrollTrigger
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { SplitText } from "gsap/SplitText";
-
-gsap.registerPlugin(Observer,ScrambleTextPlugin,ScrollTrigger,ScrollSmoother,SplitText);
+"use client";
+import { LenisProvider } from "@/context/LenisContext";
 
 
 export default function Home() {
 
-  useGSAP(()=>{
-    // ScrollSmoother.create();
-  })
 
   return (
-      <div id="content">
-        <article className="main-content relative  border-2 bg-white  border-black  flex justify-center items-center h-svh w-dvw ">
-          <h1 className="text-center text-4xl font-serif">
-            who is this crazed man
-          </h1>
-        </article>
-        {/* main footer container should have any height wanted */}
-        <footer className="h-svh w-dvw flex flex-col justify-end relative -z-10 bg-black">
-          {/* spacing should be calculation of footer? + some height in our case full height */}
-          <div className="h-[200vh] relative shrink-0 border-2">
-            {/* actual container conent goes in side this div */}
-            <div className="h-svh w-full fixed bg-blue-600 flex justify-center items-center">
-              <h1 className="font-serif text-2xl text-white">
-                im talking about my girl &lt;3
-              </h1>
-            </div>
+    <div id="content">
+      <article className="main-content bg-white    flex justify-center items-center h-svh  ">
+        <h1 className="text-center text-4xl font-serif">
+          who is this crazed man
+        </h1>
+      </article>
+      {/* <article className="main-content bg-pink-50    flex justify-center items-center h-svh  ">
+        <h1 className="text-center text-4xl font-serif">
+          its not me is it?
+        </h1>
+      </article> */}
+      {/* main footer container should have any height wanted */}
+      <footer className="h-svh flex flex-col justify-end relative -z-10 bg-black">
+        {/* spacing should be calculation of footer? + some height in our case full height */}
+        <div className="h-[200vh] relative shrink-0 ">
+          {/* actual container conent goes in side this div */}
+          <div className="h-svh w-full fixed bg-blue-600 flex justify-center items-center">
+            <h1 className="font-serif text-2xl text-white">
+              lord have mercy. 
+            </h1>
           </div>
-        </footer>
-      </div>
+        </div>
+      </footer>
+    </div>
   );
 }
---
+++ src/context/LenisContext.jsx
@@ -0,0 +1,58 @@
+// src/context/LenisContext.jsx
+"use client";
+import { createContext, useContext, useEffect, useState, useRef } from "react";
+import Lenis from "lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "../app/globals.css" 
+
+// Register GSAP plugins
+gsap.registerPlugin(ScrollTrigger);
+
+const LenisContext = createContext({
+  lenis: null,
+});
+
+export function LenisProvider({ children }) {
+  const [lenis, setLenis] = useState(null);
+  const reqIdRef = useRef(null);
+
+  useEffect(() => {
+    const lenisInstance = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      smooth: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
+    });
+
+    setLenis(lenisInstance);
+
+    // Connect Lenis to ScrollTrigger
+    lenisInstance.on('scroll', ScrollTrigger.update);
+
+    // Add Lenis raf to GSAP's ticker
+    gsap.ticker.add((time) => {
+      lenisInstance.raf(time * 1000);
+    });
+
+    // Disable lag smoothing in GSAP
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenisInstance.destroy();
+      gsap.ticker.remove(lenisInstance.raf);
+    };
+  }, []);
+
+  return (
+    <LenisContext.Provider value={{ lenis }}>
+      {children}
+    </LenisContext.Provider>
+  );
+}
+
+export const useLenis = () => {
+  return useContext(LenisContext);
+};
