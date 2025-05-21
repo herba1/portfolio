a3fc98432e7d6ca0c3a03abc9ee55fe64d2a0f3f
herbart hernandez <burnedcard77@gmail.com>
2025-05-21 16:22:37 -0700

    init

--- src/app/ui/Navigation/NavLogo.jsx
+++ src/app/ui/Navigation/NavLogo.jsx
@@ -5,7 +5,7 @@ export default function NavLogo() {
   return (
     <Link href={"#"} className=" nav__logo">
       {/* logo would go in here */}
-      <h1 className="font-extrabold text-2xl">herb.</h1>
+      <h1 className="font-extrabold text-2xl">Moises Gante</h1>
     </Link>
   );
 }
