53c0a9f36968cee902081b49e3b1e10019f89b0f
herba1 <burnedcard77@gmail.com>
2025-05-29 01:34:28 -0700

    split wip

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -10,21 +10,73 @@ gsap.registerPlugin(SplitText);
 export default function LinkMask() {
   const container = useRef();
 
-  const { contextSafe } = useGSAP(() => {}, {
+  const { contextSafe } = useGSAP(() => {
+    let textSplits = SplitText.create('.animate__line--text',{type:'lines,chars',mask:'lines'});
+    let titleSplits = SplitText.create('.animate__line--title',{type:'lines,chars',mask:'lines'});
+    let splitTl = gsap.timeline();
+    splitTl.from(textSplits.lines, {
+      yPercent:100,
+      duration:1,
+      stagger:0.05,
+      ease:'power4.out',
+    },'start')
+    .from(titleSplits.lines,{
+      yPercent:100,
+      duration:1,
+      stagger:0.05,
+      ease:'power4.out',
+    },'start+=0.5')
+    return(()=>{
+      if(textSplits)textSplits.revert();
+      if(titleSplits)titleSplits.revert();
+      if(splitTl)splitTl.kill();
+    })
+  }, {
     scope: container,
     dependencies: null,
   });
-
-  const onEnter = contextSafe((e) => {});
-
-  const onLeave = contextSafe(() => {});
 
   return (
     <div
       ref={container}
       className="w-dvw h-dvh flex justify-center items-center"
     >
-      <button>Hello !</button>
+      <section>
+        <h3 className="animate__line--title">one</h3>
+        <ul className="animate__line--text">
+          <li className="">Somewhere</li>
+          <li>
+            <p>Where one may</p>
+          </li>
+          <li>
+            <span>DO STUFF</span>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3 className="animate__line--title">one</h3>
+        <ul className="animate__line--text">
+          <li className="">Somewhere</li>
+          <li>
+            <p>Where one may</p>
+          </li>
+          <li>
+            <span>DO STUFF</span>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3 className="animate__line--title">one</h3>
+        <ul className="animate__line--text">
+          <li className="">Somewhere</li>
+          <li>
+            <p>Where one may</p>
+          </li>
+          <li>
+            <span>DO STUFF</span>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
