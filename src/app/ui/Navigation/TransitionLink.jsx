eff1c63cc13be0942f697cf552a91ddc3acaf21e
herbart hernandez <burnedcard77@gmail.com>
2025-06-21 15:25:11 -0700

    transition

--- src/app/ui/Navigation/TransitionLink.jsx
+++ src/app/ui/Navigation/TransitionLink.jsx
@@ -8,41 +8,57 @@ import { useRef } from "react";
 import Link from "next/link";
 
 export default function TransitionLink({ href = "/", children }) {
-    const router = useRouter();
-    const container = useRef(null);
-    const tl = useRef(null);
-    const pathName = usePathname();
+  const router = useRouter();
+  const container = useRef(null);
+  const tl = useRef(null);
+  const pathName = usePathname();
 
-    const {contextSafe} = useGSAP(()=>{
-        tl.current = gsap.timeline({paused:true});
-        
-        tl.current.to('.screen--out',{
-            clipPath:'inset(0% 0% 0% 0%)',
-            ease:'power3.out',
-        }).to('.screen--out',{
-            duration:1,
-            onComplete:pushRouter,
+  const { contextSafe } = useGSAP(
+    () => {
+      tl.current = gsap.timeline({ paused: true });
+      let tl2 = gsap.timeline();
+
+      tl2.fromTo(
+        ".screen--out",
+        { opacity: 1, pointerEvents: "auto" },
+        { opacity: 0, pointerEvents: "none", ease: "power3.out" }
+      );
+
+      tl.current
+        .to(".screen--out", {
+          opacity: 1,
+          ease: "power3.out",
+          pointerEvent: "none",
         })
+        .to(".screen--out", {
+          duration: 0,
+          onComplete: pushRouter,
+        });
+    },
+    { scope: container.current }
+  );
 
-    },{scope:container.current})
+  const handleClick = contextSafe((e) => {
+    e.preventDefault();
+    if (href === pathName) return;
+    tl.current.play();
+    console.log("handleClick");
+  });
 
-
-    const handleClick = contextSafe((e)=>{
-        e.preventDefault();
-        if(href === pathName) return;
-        tl.current.play();
-        console.log('handleClick');
-    })
-
-    const pushRouter = ()=>{
-        // pushing
-        router.push(href);
-    }
+  const pushRouter = () => {
+    // pushing
+    router.push(href);
+  };
 
   return (
     <div ref={container} className="inline-block">
-        <div style={{clipPath:'inset(100% 0% 0% 0%)'}} className=" screen--out w-full h-lvh absolute top-0 left-0 bg-black"></div>
-      <Link onClick={handleClick} href={href}>{children}</Link>
+      <div
+        // style={{ opacity: 0 }}
+        className=" screen--out w-full h-lvh absolute top-0 left-0 pointer-events-none bg-white"
+      ></div>
+      <Link onClick={handleClick} href={href}>
+        {children}
+      </Link>
     </div>
   );
 }
