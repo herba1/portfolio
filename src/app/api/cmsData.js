7b9bb778f421f61a24524a618f0683a61e14f6bb
herba1 <burnedcard77@gmail.com>
2025-06-30 21:55:30 -0700

    images

--- src/app/api/cmsData.js
+++ src/app/api/cmsData.js
@@ -1,8 +1,42 @@
 import { client } from "@/sanity/lib/client";
 
-export async function getProjects() {
+export async function getProjects1() {
   const data = await client.fetch(`
        *[_type == "project"] | order(completedDate desc)
+       `);
+  return data;
+}
+
+export async function getProjects() {
+  const data = await client.fetch(`
+      *[_type == "project"] | order(completedDate desc){
+      year,
+      name,
+      testimony,
+      description,
+        githubLink,
+        visitLink,
+        work,
+        tech,
+      images[] {
+    _key,
+    asset-> {
+      _id,
+      url,
+      metadata {
+        dimensions {
+          width,
+          height,
+          aspectRatio
+        },
+        lqip,
+        blurHash
+      }
+    },
+    alt
+  }
+
+  }
        `);
   return data;
 }
--- src/app/ui/Porotflio/Portoflio.jsx
+++ src/app/ui/Porotflio/Portoflio.jsx
@@ -1,36 +1,65 @@
 import { getProjects } from "@/app/api/cmsData";
 import { instrumentSerif, inter } from "@/app/fonts";
-import { ExternalLink, Plus} from "lucide-react";
+import { ExternalLink, Plus } from "lucide-react";
 import Image from "next/image";
+import { PortableText } from "next-sanity";
 import { ConfigResolutionError } from "sanity";
+import { urlFor } from "@/sanity/lib/image";
 
-function ProjectHeader({ className = "", name, year, work }) {
+function ProjectHeader({ className = "", data }) {
+  const { name, year, work } = data;
+
   return (
     <div className="text-dark hover:bg-dark hover:text-light heading grid grid-cols-4 gap-4 py-2 transition-all duration-500 ease-[cubic-bezier(0,1.11,.53,.95)] hover:px-2 md:grid-cols-12 md:gap-6">
-      <h3 className="tracking-body-base md:col-span-4">Linux-Lab</h3>
+      <h3 className="tracking-body-base md:col-span-4">{name}</h3>
       <h3 className="tracking-body-base hidden md:col-span-4 md:block">
-        Frontend Developer, Designer
+        {work.join(", ")}
       </h3>
-      <h3 className="tracking-body-base col-start-3 md:col-span-1">2025</h3>
+      <h3 className="tracking-body-base col-start-3 md:col-span-1">{year}</h3>
       <Plus className="justify-self-end md:col-start-12"></Plus>
     </div>
   );
 }
 
-function ProjectGallery({ className = "", images = [] }) {
+function ProjectGallery({ className = "", data }) {
   // add images sanity cms
+  const images = data.images.map((image) => {
+    console.log(image);
+    console.log(
+      urlFor(image)
+        .width(image.asset.metadata.dimensions.width)
+        .height(image.asset.metadata.dimensions.height)
+        .url(),
+    );
+    return (
+      <Image
+        key={image.alt}
+        src={urlFor(image)
+          .width(image.asset.metadata.dimensions.width)
+          .height(image.asset.metadata.dimensions.height)
+          .url()}
+        className="col-span-full aspect-video w-full rounded-2xl object-cover md:col-span-6 lg:col-span-4 xl:col-span-3"
+        alt={image.alt}
+        height={image.asset.metadata.dimensions.height}
+        width={image.asset.metadata.dimensions.width}
+        placeholder="blur"
+        blurDataURL={image.asset.metadata.lqip}
+      ></Image>
+    );
+  });
   return (
     <div
       className={`grid min-h-fit grid-cols-12 gap-4 overflow-clip ${className}`}
     >
-      <Image
+      {images}
+      {/* <Image
         src={"/1.png"}
         className="col-span-full aspect-video w-full rounded-2xl object-cover md:col-span-6 lg:col-span-4 xl:col-span-3"
         alt="img"
         height={300}
         width={300}
         // style={{imageRendering:'pixelated'}}
-      ></Image>
+      ></Image> */}
     </div>
   );
 }
@@ -52,20 +81,21 @@ function ProjectTestimony({
   );
 }
 
-function ProjectContent({ className = "" }) {
+function ProjectContent({ className = "", data }) {
+  console.log(data);
   return (
     <div className="content flex flex-col gap-10 py-5 lg:gap-6">
       <h3
         className={` ${instrumentSerif.className} tracking-heading-mobile text-5xl lg:text-6xl`}
       >
-        Linux-Lab
+        {data.name}
       </h3>
       <div className="content__body flex flex-col gap-10 lg:flex-row lg:gap-6">
-        <ProjectDescription></ProjectDescription>
-        <ProjectMetadata></ProjectMetadata>
+        <ProjectDescription data={data}></ProjectDescription>
+        <ProjectMetadata data={data}></ProjectMetadata>
       </div>
-      <ProjectTestimony></ProjectTestimony>
-      <ProjectGallery></ProjectGallery>
+      {data.testimony && <ProjectTestimony>{data.testimony}</ProjectTestimony>}
+      <ProjectGallery data={data}></ProjectGallery>
     </div>
   );
 }
@@ -73,14 +103,9 @@ function ProjectContent({ className = "" }) {
 export function ProjectDescription({ className = "", data }) {
   return (
     <article
-      className={`content__desc tracking-body-base flex flex-1/2 flex-col gap-4 leading-normal ${inter.className}`}
+      className={`content__desc tracking-body-base flex flex-1/2 flex-col gap-4 leading-normal font-normal ${inter.className}`}
     >
-      <p>
-        Throughout the development process, I created custom state management
-        systems and interactive elements, working efficiently to deliver a fully
-        functional platform. This approach showcased my ability to build complex
-        web applications using pure web fundamentals and custom-coded solutions.
-      </p>
+      <PortableText value={data.description}></PortableText>
     </article>
   );
 }
@@ -89,27 +114,32 @@ export function ProjectMetadata({ className = "", data }) {
   return (
     <div className="content__extra flex flex-1/2 flex-col justify-between gap-10">
       <div className="content__links flex justify-between lg:order-1">
-        <a className="flex font-bold" href="#">
+        <a
+          className={`flex font-bold ${!data.githubLink && "pointer-events-none opacity-0"}`}
+          href={data.githubLink}
+        >
           Github <ExternalLink></ExternalLink>
         </a>
-        <a className="flex font-bold" href="#">
+
+        <a
+          className={`flex font-bold ${!data.visitLink && "pointer-events-none opacity-0"}`}
+          href={data.visitLink}
+        >
           Visit Site <ExternalLink></ExternalLink>
         </a>
       </div>
       <ul className="content__metadata flex grid-cols-6 flex-col gap-4 lg:grid">
         <li className="col-span-3 flex flex-col">
           <span className="text-dark/70">Date</span>
-          <span className="">2025</span>
+          <span className="">{data.year}</span>
         </li>
         <li className="col-span-3 flex flex-col">
           <span className="text-dark/70">Tech</span>
-          <span className="">
-            Vanilla JS, HTML, CSS, Figma, GSAP, TailwindCSS
-          </span>
+          <span className="">{data.tech.join(", ")}</span>
         </li>
         <li className="col-span-6 flex flex-col">
           <span className="text-dark/70">Services</span>
-          <span className="">Frontend Development, Web Design</span>
+          <span className="">{data.work.join(", ")}</span>
         </li>
       </ul>
     </div>
@@ -117,22 +147,19 @@ export function ProjectMetadata({ className = "", data }) {
 }
 
 export function ProjectItem({ className = "", data }) {
-  console.log(data);
   return (
     <li className="relative min-h-fit font-medium">
-      <ProjectHeader></ProjectHeader>
-      <ProjectContent></ProjectContent>
-      <div className="border-anim border-dark pointer-events-none absolute top-0 left-0 h-full w-full border-b-2"></div>
+      <ProjectHeader data={data}></ProjectHeader>
+      <ProjectContent data={data}></ProjectContent>
+      <div className="border-animated border-dark pointer-events-none absolute top-0 left-0 h-full w-full border-b-2"></div>
     </li>
   );
 }
 
-export function ProjectList({ className = "",data }) {
-
-  // console.log(data);
-  const items = data.map((item)=>{
-    return(<ProjectItem data={item}></ProjectItem>)
-  })
+export function ProjectList({ className = "", data }) {
+  const items = data.map((item) => {
+    return <ProjectItem key={item.name} data={item}></ProjectItem>;
+  });
 
   return (
     <ul className="project__list">
@@ -156,7 +183,6 @@ export function ProjectList({ className = "",data }) {
 
 export default async function Portoflio() {
   const projects = await getProjects();
-  // console.log(projects)
 
   return (
     <section className="h-fit w-full">
