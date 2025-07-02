3dc804ec7ff175b9464c19f09efef05d50e492e2
herba1 <burnedcard77@gmail.com>
2025-07-01 23:13:08 -0700

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,11 @@ export const metadata = {
     index: true,
     follow: true,
   },
-  viewport: 'width=device-width, initial-scale=1',
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
 }
 
 export default function RootLayout({ children }) {
